153fa873204eaaa0bc67e6ac08d83c9e6ac0db78
yafeiya <jubary@njust.edu.cn>
2023-08-23 01:14:33 +0800

    yang
    delete electron

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import electron from 'vite-plugin-electron'
-import proxy from 'vite-plugin-proxy'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    electron({
-      entry: 'electron/main.js'
-  },
-  )
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-
-    
-
-  })
+  // server:{
+  //   host:'192.168.0.102'
+  // }
+})
