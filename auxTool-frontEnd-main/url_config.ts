f19bf38440aa9a0db589bc12a51ac9484fa6b4bb
yafeiya <102225985+yafeiya@users.noreply.github.com>
2023-10-15 19:07:50 +0800

    Update url_config.ts

--- auxTool-frontEnd-main/url_config.ts
+++ auxTool-frontEnd-main/url_config.ts
@@ -1,16 +1,3 @@
-// export function EndUrl(){
-//   const url_dict =
-//   {
-//     // 局域网内暴露本机地址
-//     "frontEndUrl":'127.0.0.1',
-//     "frontpost":"5173",
-//     // 向后端发送请求的地址+端口
-//     "backEndUrl": 'http://127.0.0.1:8080',
-//     "fileUrl": 'http://127.0.0.1:5173'
-// }
-//     return url_dict
-//
-// }
 export function EndUrl(){
     const url_dict =
         {
